f6716b4f989f185b3c636434de47d30d227c051d
davidjac977 <davidjacinto2006@gmail.com>
2024-08-17 16:37:18 -0300

    Add files via upload

---
+++ aula-12/script.js
@@ -0,0 +1,87 @@
+// const num = +prompt('Digite o número: ')
+
+// if (num === 0) {
+//     console.log('O número é igual a zero!')
+// } if (num < 0) {
+//     console.log('O número é negativo!')
+// } if (num > 0) {
+//     console.log('O número é positivo')
+// }
+
+// const dia = +prompt('Digite o dia: ')
+// const mes = +prompt('Digite o mês: ')
+// const ano = +prompt('Digite o ano: ')
+
+// if (ano < 0) {
+//     console.log('A data é inválida!')
+// } if ((dia <= 31 && mes === 1) ?? mes === 3 ?? mes === 5 ?? mes === 7 ?? mes === 8 ?? mes === 10 ?? mes === 12) {
+//     console.log('A data é válida!')
+// } if (mes >= 13) {
+//     console.log('A data é inválida!')
+// } if (mes === 2 && dia >= 29) {
+//     console.log('A data é inválida!')
+// } if (mes === 2 && dia <= 28) {
+//     console.log('A data é válida!')
+// } if ((dia <= 30 && mes === 1) ?? mes === 3 ?? mes === 5 ?? mes === 7 ?? mes === 8 ?? mes === 10 ?? mes === 12) {
+//     console.log('A data é inválida!')
+// } if ((dia <= 30 && mes === 4) ?? mes === 6 ?? mes === 9 ?? mes === 11) {
+//     console.log('A data é válida!')
+// } if ((dia === 31 && mes === 4) ?? mes === 6 ?? mes === 9 ?? mes === 11) {
+//     console.log('A data é inválida')
+// } if ((dia === 30 && mes === 1) ?? mes === 3 ?? mes === 5 ?? mes === 7 ?? mes === 8 ?? mes === 10 ?? mes === 12) {
+//     console.log('A data é inválida!')
+// } if (dia > 31) {
+//     console.log('A data é inválida')
+// } 
+
+// const idade = +prompt('Digite sua idade: ')
+
+// if (idade <= 11) {
+//     console.log('Criança')
+// } if (idade >= 12 && idade < 18) {
+//     console.log('Adolescente')
+// } if (idade >= 18 && idade < 60) {
+//     console.log('Adulto')
+// } if (idade >= 60) {
+//     console.log('Idoso')
+// }
+
+
+// const num1 = +prompt('Digite sua primeira nota: ')
+// const num2 = +prompt('Digite sua segunda nota: ')
+// const num3 = +prompt('Digite sua terceira nota: ')
+// const media = (num1 + num2 + num3) / 3 
+
+// console.log(media) 
+
+// if (media >= 7) {
+//     console.log('Aprovado!')
+// } if (media >= 5 && media < 7) {
+//     console.log('Em recuperação!')
+// } if (media < 5) {
+//     console.log('Reprovado!')
+// } 
+
+// const nome = prompt('Digite seu nome: ')
+ 
+// const letra1 = nome.split("")[0]
+// console.log(letra1)
+// if (letra1 === 'a' || letra1 === 'A') {
+//     console.log('Começa com A!')
+// } else {
+//     console.log('Não começa com A!')
+// }
+
+// const palavra = prompt('Digite sua palavra: ')
+
+// const versa =  JSON.stringify(palavra.split(""))
+// const reversa = JSON.stringify(palavra.split("").reverse())
+
+// console.log(versa, reversa)
+
+// if (versa === reversa) {
+//     console.log('É palíndromo!')
+// } else {
+//     console.log('Não é palíndromo!')
+// }
+
