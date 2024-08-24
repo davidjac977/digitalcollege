1c70c80ac82ccaadaef1959b28ec7ed2c33110e7
davidjac977 <davidjacinto2006@gmail.com>
2024-08-24 16:54:52 -0300

    Add files via upload

---
+++ aula-13/script.js
@@ -0,0 +1,73 @@
+// let index = 0
+
+// while (index <= 40) {
+//     console.log(index)
+//     index = index + 1
+// } 
+
+// do {
+//     console.log(index)
+//     index += 1
+// } while (index <= 20)
+
+// for (let i = 0; i <= 10; i++) { console.log(i) }
+
+
+// for (let i = 10; i >= 1; i-- ) { console.log(i) }
+
+// for (let i = 0; i <= 20; i++ ) if ( i % 2 === 0) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 20; i++ ) if ( i % 2 !== 0) {
+//     console.log(i)
+// }
+
+// let soma = 0
+
+// for (let i = 1; i <= 100; i++)  {
+//     soma += i 
+// } 
+
+// console.log(soma)
+
+
+// const num = +prompt('Digite um número: ')
+// let fatorial = 1
+// for (let i = num; i >= 1; i--) {
+//     fatorial *= i
+// }
+// console.log(fatorial)
+
+    // const frutas = ['Banana', 'Caju', 'Abacaxi', 'Figo', 'Damasco', 'Embaúba']
+    // const numeros = [12, 3, 20, 7, 32, 1]
+    // const pessoas = [
+    // {
+    //     nome: 'David',
+    //     idade: 18
+    // }    
+        
+    // ]
+    // console.log(typeof frutas)
+    // 'texto'. split()
+    // for(const fruta of frutas) {
+    //     console.log(fruta)
+    // } 
+    
+//     const palavra = prompt('Digite uma palavra: ').toUpperCase()
+//     const letras = palavra.split("")
+//   let contador = 0
+//   const vogais = ['A', 'E', 'I', 'O', 'U']
+    
+ 
+// for (const letra of letras) {
+//     if (vogais.includes(letra)) {
+//         contador++
+//     }
+// } 
+// console.log(contador)
+
+// const palavra = prompt('Digite uma palavra: ')
+
+// const reversa = palavra.split("").reverse()
+// console.log(palavra, reversa)
