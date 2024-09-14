63bf25a6ea52a004da0e2645731afe9e19bda53e
davidjac977 <davidjacinto2006@gmail.com>
2024-09-14 16:44:13 -0300

    Add files via upload

---
+++ aula-16/script.js
@@ -0,0 +1,16 @@
+// function calcularMedia(notas) {
+//     let soma = 0
+
+//       for (const nota of notas) { soma += nota }
+//     return (soma / notas.length) 
+// }
+//     const media = calcularMedia([5, 6, 8, 10, 9])
+    
+//     console.log(media)
+
+// const valores = [5, 6, 8, 10, 9]
+// const dobro = valores.map(num => num * 2)
+// console.log(dobro)
+
+const nomes = [ 'Ana', 'Carlos', 'Beatriz', 'Daniel'] 
+nomes.forEach((nomes) => console.log (nomes))
